4072a71e2986434cdfd2f963d74828c4726a56b2
fadhili josue <fadhilijosue27@gmail.com>
2023-02-04 11:34:42 +0200

    dd

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-<<<<<<< HEAD
-const http = require('http');
-const fs = require("fs")
+
+// const http = require('http');
+// const fs = require("fs")
 
 
-const homePage = fs.readFileSync('./homePage.html', 'utf-8')
-const loginForm = fs.readFileSync("login.html", 'utf-8')
-const server = http.createServer((request, response) => {
-    const path = request.url;
+// const homePage = fs.readFileSync('./homePage.html', 'utf-8')
+// const loginForm = fs.readFileSync("login.html", 'utf-8')
+// const server = http.createServer((request, response) => {
+//     const path = request.url;
 
 
-    if (path === "/" || path === "/login") {
-        response.end(loginForm)
-    }
-    else if (path === "/logout") { response.end(loginForm) }
-    else if (path === "/homePage") { response.end(homePage) }
-    else if (path === '/products') {
-        response.end('<h1>you are in the products page</h1>')
-    }
-    else { response.end('<h1>Page Not Found</h1><hr>') }
-})
+//     if (path === "/" || path === "/login") {
+//         response.end(loginForm)
+//     }
+//     else if (path === "/logout") { response.end(loginForm) }
+//     else if (path === "/homePage") { response.end(homePage) }
+//     else if (path === '/products') {
+//         response.end('<h1>you are in the products page</h1>')
+//     }
+//     else { response.end('<h1>Page Not Found</h1><hr>') }
+// })
 
-server.listen(5000, () => {
-    console.log('server started')
-})
-=======
+// server.listen(5000, () => {
+//     console.log('server started')
+// })
+
 const express = require("express");
-const app =  express();
 const bcrypt = require("bcrypt")
-const connectDB = require('./config/db');
-connectDB();
+const app =  express();
+// const connectDB = require('./config/db');
+// connectDB();
 // const ejsLayouts = require("express-ejs-layouts");
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -37,29 +37,29 @@ app.use(express.urlencoded({extended:false}))
 
 // app.set("view engine","ejs");
 // app.set("view", __dirname+"views");
-// app.set("layout","./views/layouts/layout.ejs")
+// app.set("layout","layouts/layout.ejs")
 // app.use(ejsLayouts)
 
 const users = [];
 
-app.post('/register', async (req,res) => {
-    try {
-        hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push(
-            {
-                id: Date.now().toString(),
-                name: req.body.name,
-                email: req.body.email,
-                password: hashedPassword
-            }
-        )
-        console.log(req.body.name);
-        // console.log(users);
-    } catch (error) {
-        console.error();
-        res.redirect("/register")
-    }
-})
+// app.post('/register', async (req,res) => {
+//     try {
+//         hashedPassword = await bcrypt.hash(req.body.password, 10)
+//         users.push(
+//             {
+//                 id: Date.now().toString(),
+//                 name: req.body.name,
+//                 email: req.body.email,
+//                 password: hashedPassword
+//             }
+//         )
+//         console.log(req.body.name);
+//         // console.log(users);
+//     } catch (error) {
+//         console.error();
+//         res.redirect("/register")
+//     }
+// })
 
 
 // const ejsLayouts = require("express-ejs-layouts");
@@ -75,8 +75,8 @@ app.use('/register',(req,res) => {
     res.render('register.ejs') 
 })
 
-app.post('/o',(req,res) => {
-        res.status(200).json({mesage:`${req.body.id}`})}) 
+// app.post('/o',(req,res) => {
+//         res.status(200).json({mesage:`${req.body.id}`})}) 
 
 app.listen(4000)
->>>>>>> 543f0d5a82940efb0a4286a9d067930c41129ac5
+
