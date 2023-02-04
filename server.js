f9b9598c787e5f811c893a661dffbb0843d52d2e
fadhili josue <fadhilijosue27@gmail.com>
2023-02-04 13:00:36 +0200

    using passport

--- server.js
+++ server.js
@@ -1,70 +1,33 @@
-
-// const http = require('http');
-// const fs = require("fs")
-
-
-// const homePage = fs.readFileSync('./homePage.html', 'utf-8')
-// const loginForm = fs.readFileSync("login.html", 'utf-8')
-// const server = http.createServer((request, response) => {
-//     const path = request.url;
-
-
-//     if (path === "/" || path === "/login") {
-//         response.end(loginForm)
-//     }
-//     else if (path === "/logout") { response.end(loginForm) }
-//     else if (path === "/homePage") { response.end(homePage) }
-//     else if (path === '/products') {
-//         response.end('<h1>you are in the products page</h1>')
-//     }
-//     else { response.end('<h1>Page Not Found</h1><hr>') }
-// })
-
-// server.listen(5000, () => {
-//     console.log('server started')
-// })
-
 const express = require("express");
 const bcrypt = require("bcrypt")
+const passport = require('passport');
 const app =  express();
-// const connectDB = require('./config/db');
-// connectDB();
-// const ejsLayouts = require("express-ejs-layouts");
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-
-
-// app.set("view engine","ejs");
-// app.set("view", __dirname+"views");
-// app.set("layout","layouts/layout.ejs")
-// app.use(ejsLayouts)
-
 const users = [];
 
-// app.post('/register', async (req,res) => {
-//     try {
-//         hashedPassword = await bcrypt.hash(req.body.password, 10)
-//         users.push(
-//             {
-//                 id: Date.now().toString(),
-//                 name: req.body.name,
-//                 email: req.body.email,
-//                 password: hashedPassword
-//             }
-//         )
-//         console.log(req.body.name);
-//         // console.log(users);
-//     } catch (error) {
-//         console.error();
-//         res.redirect("/register")
-//     }
-// })
+app.post('/register', async (req,res) => {
+    try {
+        hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push(
+            {
+                id: Date.now().toString(),
+                name: req.body.full_name,
+                email: req.body.email,
+                password: hashedPassword
+            }
+        )
+        console.log(users)
+            res.redirect("/login")
+    } catch (error) {
+        console.log(error);
+        res.redirect("/register")
+    }
+})
+console.log(users);
 
-
-// const ejsLayouts = require("express-ejs-layouts");
-// app.set('views', __dirname+"views")
-// app.set('layouts', 'layouts/layouts')
 app.use('/home',(req,res) => {
     res.render('home.ejs') 
 })
@@ -74,9 +37,6 @@ app.use('/login',(req,res) => {
 app.use('/register',(req,res) => {
     res.render('register.ejs') 
 })
-
-// app.post('/o',(req,res) => {
-//         res.status(200).json({mesage:`${req.body.id}`})}) 
 
 app.listen(4000)
 
